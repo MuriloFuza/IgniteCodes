f4fee48d49f29e04f39b7e31da82cf241edb67f3
MuriloFuza <muriloacademix@gmail.com>
2021-03-08 17:33:12 -0400

    Method GET - Extrato

--- BancoAPI/src/index.js
+++ BancoAPI/src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -32,6 +33,18 @@ app.post('/account', (request, response) => {
 
   console.log(customers);
   return response.status(201).json({ message: 'Criado com sucesso!' });
+})
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Costumer not found!' })
+  }
+
+  return response.json(customer.statement);
 })
 
 app.listen(3333);
