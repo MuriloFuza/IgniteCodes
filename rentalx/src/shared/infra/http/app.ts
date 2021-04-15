59d5745e0b1623f630e120366319f3392a61924b
MuriloFuza <muriloacademix@gmail.com>
2021-04-15 11:23:24 -0400

    Update Code - Module IV Completed

--- rentalx/src/shared/infra/http/app.ts
+++ rentalx/src/shared/infra/http/app.ts
@@ -28,4 +28,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
   return response.status(500).json({ status: 'error', message: `Internal server error - ${err.message}` });
 });
 
-app.listen(3333, () => console.log('server is running!'));
+export { app };
