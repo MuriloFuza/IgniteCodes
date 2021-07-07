38c190423a442a16e11080b4ab1efd32a2623377
MuriloFuza <muriloacademix@gmail.com>
2021-07-07 14:07:41 -0400

    Gerando certificado em pdf local

--- serverless/ignitecertificate/src/functions/generateCertificate.ts
+++ serverless/ignitecertificate/src/functions/generateCertificate.ts
@@ -1,9 +1,30 @@
-import {document} from '../utils/dynamodbClient';
+import chromium  from 'chrome-aws-lambda'
+import {document} from '../utils/dynamodbClient'
+import path from 'path'
+import fs from 'fs'
+import Handlebars from 'handlebars'
+import dayjs from 'dayjs'
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async function( data: ITemplate ){
+  const filePath = path.join(process.cwd(), 'src', 'templates', 'certificate.hbs');
+
+  const html = fs.readFileSync(filePath, 'utf-8');
+
+  return Handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -17,6 +38,53 @@ export const handle = async (event) => {
       grade
     },
   }).promise();
+
+  //Gerar certificado
+  //Compilar usando o handlebars
+
+  const medalPath = path.join(process.cwd(), 'src', 'templates', 'selo.png');
+  const medal = fs.readFileSync(medalPath, 'base64');
+
+  const data: ITemplate = {
+    date: dayjs().format('DD/MM/YYYY'),
+    grade,
+    name,
+    id,
+    medal
+  }
+
+  const content  = await compile(data);
+
+  //Transformando em pdf
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  });
+
+
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+
+  const pdf = await page.pdf({
+    format: 'a4',
+    landscape: true,
+    path: process.env.IS_OFFLINE ? 'certificate.pdf' : null,
+    printBackground: true,
+    preferCSSPageSize: true
+  });
+
+
+  console.log("Passou ");
+  await browser.close();
+
+  //salvar no S3
+
+
 
   return {
     statusCode: 201,
--- serverless/ignitecertificate/src/utils/dynamodbClient.ts
+++ serverless/ignitecertificate/src/utils/dynamodbClient.ts
@@ -2,7 +2,9 @@ import {DynamoDB} from 'aws-sdk'
 
 const options = {
   region: 'localhost',
-  endpoint: 'http://localhost:8000'
+  endpoint: 'http://localhost:8000',
+  accessKeyId: 'AKIATHTBPALBASOZYEXV', 
+  secretAccessKey: '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',
 }
 
 const isOffline = () => {
